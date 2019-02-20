8e3efd9f6648ad3111429098f38fa589c395cafa
Guowei Lv <lvguowei1002@gmail.com>
2019-02-20 21:38:16 +0200

    chapter 2 1

---
+++ chapter2/1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+  printf("%s\n", "       *");
+  printf("%s\n", "      * ");
+  printf("%s\n", "     *  ");
+  printf("%s\n", "*   *   ");
+  printf("%s\n", " * *    ");
+  printf("%s\n", "  *     ");
+}
