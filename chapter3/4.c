0a48a0f09b2b16d027cf0b19959679e1974921df
Guowei Lv <lvguowei1002@gmail.com>
2019-02-22 21:28:23 +0200

    3.4

---
+++ chapter3/4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c;
+  printf("Enter phone number [(xxx) xxx-xxxx]: ");
+  scanf("(%d) %d-%d", &a, &b, &c);
+  printf("You entered %d.%d.%d\n", a, b, c);
+}
