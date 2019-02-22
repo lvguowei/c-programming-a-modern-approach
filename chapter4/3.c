ecc5e25c9458cc20ab27decd657318164d75e6a1
Guowei Lv <lvguowei1002@gmail.com>
2019-02-22 22:55:04 +0200

    4.3

---
+++ chapter4/3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int n1, n2, n3;
+  printf("Enter a three-digit number: ");
+  scanf("%1d%1d%1d", &n1, &n2, &n3);
+  printf("The reversal is: %d%d%d\n", n3, n2, n1);
+}
