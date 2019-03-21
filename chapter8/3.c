500e1c7be8ec5863ec7358cbd5c082d49e7efa2f
Guowei Lv <lvguowei1002@gmail.com>
2019-03-21 22:38:46 +0200

    remove some

--- chapter8/3.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int main() {
-  int weekend[7] = {true, false, false, false, false, false, true};
-}
--- chapter8/4.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int main() {
-  int weekend[7] = {[0] = true, [6] = true};
-}
