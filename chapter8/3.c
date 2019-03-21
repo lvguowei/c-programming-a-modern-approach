db0471e521fbcb9d451fa6ed0690d37152cfd102
Guowei Lv <lvguowei1002@gmail.com>
2019-03-21 22:36:21 +0200

    8.3 8.4 8.5

---
+++ chapter8/3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+  int weekend[7] = {true, false, false, false, false, false, true};
+}
---
+++ chapter8/4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+  int weekend[7] = {[0] = true, [6] = true};
+}
