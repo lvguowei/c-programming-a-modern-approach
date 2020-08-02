bd91f5b3c00384bb5bb81696ebcbc87117963736
Guowei Lv <lvguowei1002@gmail.com>
2020-08-02 19:54:52 +0300

    ex16.15

---
+++ chapter16/exercises/15.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+enum week {MON, TUE, WED, THU, FRI, SAT, SUN};
+
+typedef enum {MON, TUE, WED, THU, FRI, SAT, SUN} Week;
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
