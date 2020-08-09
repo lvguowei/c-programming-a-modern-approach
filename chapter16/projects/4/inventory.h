ea41d58d1a41f95e6825a62b7310cf4c9314c94d
Guowei Lv <lvguowei1002@gmail.com>
2020-08-09 20:45:01 +0300

    16.4

---
+++ chapter16/projects/4/inventory.h
@@ -0,0 +1,15 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#define NAME_LEN 25
+#define MAX_PARTS 100
+
+struct part
+{
+  int number;
+  char name[NAME_LEN];
+  int on_hand;
+  int price;
+};
+
+#endif
