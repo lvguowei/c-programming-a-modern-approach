467399b20b0b3c04737c1ee6e0b9f7d2cbcebfc1
Guowei Lv <lvguowei1002@gmail.com>
2021-03-01 22:02:27 +0200

    ex

--- chapter17/exercises/6.c
+++ chapter17/exercises/6.c
@@ -40,25 +40,6 @@ struct node *search_list(struct node *list, int n) {
   return list;
 }
 
-/* struct node *delete_from_list(struct node *list, int n) { */
-/*   struct node *cur, *prev; */
-/*   for (cur = list, prev = NULL; cur != NULL && cur->value != n; prev = cur, cur = cur->next) */
-/*     ; */
-  
-/*   // n not found */
-/*   if (cur == NULL) { */
-/*     return list; */
-/*   } */
-
-/*   if (prev == NULL) { */
-/*     list = list->next; */
-/*   } else { */
-/*     prev->next = cur->next; */
-/*   } */
-/*   free(cur); */
-/*   return list; */
-/* } */
-
 struct node *delete_from_list(struct node *list, int n) {
   struct node *p = list;
 
